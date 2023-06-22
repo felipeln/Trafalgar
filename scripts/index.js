81c979f10b27b899e8ba174d28a617e48d91f36f
Felipeln <Felipeln@protonmail.com>
2023-06-22 16:00:59 -0300

    adding flickity carousel

--- scripts/index.js
+++ scripts/index.js
@@ -23,3 +23,58 @@ for (const link of menuLinks) {
         }
     })
 }
+
+
+
+let utils = window.fizzyUIUtils;
+
+let carousel = document.querySelector('.customers')
+
+let flkty = new Flickity (carousel, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+    autoPlay: true, // every 3s
+    
+    prevNextButtons: false,
+    pageDots: false
+})
+
+
+let  indicators = document.querySelector('.carousel-indicators')
+
+let dotButtons = utils.makeArray(indicators.querySelectorAll('.dot'))
+
+flkty.on('select', function() {
+    let previousSelectedBtn = document.querySelector('.selecionado')
+
+    let selectedBtn = indicators.querySelectorAll('.dot')[flkty.selectedIndex]
+
+    previousSelectedBtn.classList.remove('selecionado')
+    selectedBtn.classList.add('selecionado')
+})
+
+
+
+indicators.addEventListener('click', function(event) {
+    if ( !matchesSelector( event.target, '.dot' ) ) {
+        return;
+      }
+
+    let index = dotButtons.indexOf(event.target)
+    flkty.select(index)
+})
+
+
+// previous
+let previousButton = document.querySelector('.left-arrow');
+previousButton.addEventListener( 'click', function() {
+  flkty.previous();
+});
+// next
+let nextButton = document.querySelector('.right-arrow');
+nextButton.addEventListener( 'click', function() {
+  flkty.next();
+});
